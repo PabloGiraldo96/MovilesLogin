0fe457b55cd025c83e9b39fdcce645fb1db5e8f5
Juan Pablo Jaramillo Giraldo <juanpablogiraldo9602@gmail.com>
2023-04-26 00:31:01 -0500

    Se añaden archivos de StackNavigator

--- App.js
+++ App.js
@@ -1,20 +1,105 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
+
+const Stack = createStackNavigator();
+
+
+const Login = () => {
+  const navigation = useNavigation();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleLogin = () => {
+    if (username === 'usuario' && password === 'contraseña') {
+      navigation.navigate('Home');
+    } else {
+      setError('Usuario o contraseña incorrectos');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Iniciar sesión</Text>
+      <TextInput
+        label="Nombre de usuario"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        label="Contraseña"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      {error ? <Text style={styles.error}>{error}</Text> : null}
+      <Button mode="contained" onPress={handleLogin}>
+        Ingresar
+      </Button>
     </View>
   );
-}
+};
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Bienvenido a la aplicación</Text>
+    </View>
+  );
+};
+
+
+const BottomTab = createBottomTabNavigator();
+
+const MainStackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <BottomTab.Navigator>
+        <BottomTab.Screen name="Inicio" component={MainStackNavigator} />
+        <BottomTab.Screen name="Perfil" component={ProfileScreen} />
+      </BottomTab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const ProfileScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Perfil</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    padding: 20,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  error: {
+    color: 'red',
+    marginBottom: 10,
+  },
 });
+
+export default App;
